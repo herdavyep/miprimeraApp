16de83bb7c06665e21f3de97d7a1ea50dd48ca0f
heriberto david yepes <herdavyep@hotmail.com>
2016-12-27 12:57:00 -0500

    mi primera app
    no me aparece el teclado

--- miprimeraApp/ViewController.swift
+++ miprimeraApp/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var saludo: UILabel!
+    
+    @IBOutlet var nombre: UITextField!
 
+    @IBOutlet var saludo2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func botonPresionado(_ sender: UIButton) {
+        
+        let nombreUser = nombre.text!
+        
+        let alertController = UIAlertController(title: "🤗 Hola \(nombreUser) presionaste el boton", message: "Muchas gracias profesor!!", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+        saludo.text = "🤓 Bienvenido \(nombreUser) a "
+        saludo2.text = "Mi primera App 👍"
+    }
 
 }
 
